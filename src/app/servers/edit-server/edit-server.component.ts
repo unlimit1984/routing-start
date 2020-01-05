c03ac67ccb31e4db51b49377f721b7100b97f7ab
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2020-01-05 23:41:25 +0100

    Make lesson 137

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -8,13 +9,20 @@ import { ServersService } from '../servers.service';
   styleUrls: ['./edit-server.component.css']
 })
 export class EditServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number, name: string, status: string };
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    console.log('queryParams: ' + this.route.snapshot.queryParams);
+    console.log('fragment #: ' + this.route.snapshot.fragment);
+    this.route.queryParams.subscribe(/*provide arrow function here*/);
+    this.route.fragment.subscribe(/*provide arrow function here*/);
+
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
