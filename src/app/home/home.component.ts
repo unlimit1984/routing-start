a1393a423775f18bc20eaeeb44cdb93e2196fc3d
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2020-01-05 23:28:46 +0100

    Make lesson 136

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,4 +17,8 @@ export class HomeComponent implements OnInit {
     // complex calculation
     this.router.navigate(['/servers']);
   }
+
+  onLoadServer(id: number) {
+    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: 1}, fragment: 'loading'});
+  }
 }
