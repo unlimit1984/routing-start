50656df414ded94751c9eb45fee2c22969582e34
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2020-01-07 23:43:10 +0100

    Make lesson 144 - separate routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,22 +13,23 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
-const appRoutes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'users', component: UsersComponent, children: [
-      {path: ':id/:name', component: UserComponent}
-    ]
-  }
-  ,
-  {path: 'servers', component: ServersComponent, children: [
-      {path: ':id', component: ServerComponent},
-      {path: ':id/edit', component: EditServerComponent}
-    ]
-  },
-  {path: 'page-not-found', component: PageNotFoundComponent},
-  {path: '**', redirectTo: 'page-not-found'}
-];
+// const appRoutes: Routes = [
+//   {path: '', component: HomeComponent},
+//   {path: 'users', component: UsersComponent, children: [
+//       {path: ':id/:name', component: UserComponent}
+//     ]
+//   }
+//   ,
+//   {path: 'servers', component: ServersComponent, children: [
+//       {path: ':id', component: ServerComponent},
+//       {path: ':id/edit', component: EditServerComponent}
+//     ]
+//   },
+//   {path: 'page-not-found', component: PageNotFoundComponent},
+//   {path: '**', redirectTo: 'page-not-found'}
+// ];
 
 @NgModule({
   declarations: [
@@ -44,7 +45,8 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    // RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
