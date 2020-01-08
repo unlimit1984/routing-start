0386c7ee44657930f23466e65d0d78d83eb2a413
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2020-01-08 22:06:34 +0100

    Make lesson 146 - Guards for routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { ServersService } from './servers/servers.service';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
 
 // const appRoutes: Routes = [
 //   {path: '', component: HomeComponent},
@@ -48,7 +50,7 @@ import { AppRoutingModule } from './app-routing.module';
     // RouterModule.forRoot(appRoutes)
     AppRoutingModule
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule {
