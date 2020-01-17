2bbddaf0308aefbe42ebcad146c2dd40037ec719
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2020-01-17 23:32:23 +0100

    Make lesson 152 - Cleanup code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -36,7 +36,8 @@ const appRoutes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(appRoutes, {useHash: true})
+    // RouterModule.forRoot(appRoutes, {useHash: true})
+    RouterModule.forRoot(appRoutes)
   ],
   exports: [RouterModule]
 })
